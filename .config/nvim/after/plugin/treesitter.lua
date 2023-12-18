b836c99a099cf3aa8953977448e2597cc2ff9030
fvrrrn <boris.chernystrand@gmail.com>
2023-12-18 11:50:49 +0300

    feat(nvim): update tree sitter

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -3,6 +3,7 @@ if not status then
 	return
 end
 
+---@diagnostic disable-next-line: missing-fields
 configs.setup({
 	ensure_installed = {
 		"javascript",
@@ -14,20 +15,23 @@ configs.setup({
 		"lua",
 		"json",
 		"toml",
-		"yaml",
 		"bash",
-		"dockerfile",
 	},
 	sync_install = false,
 	ignore_install = { "" }, -- List of parsers to ignore installing
 	highlight = {
 		enable = true, -- false will disable the whole extension
 		disable = { "" }, -- list of language that will be disabled
-		additional_vim_regex_highlighting = true,
+		additional_vim_regex_highlighting = false,
 	},
 	indent = { enable = true, disable = { "yaml" } },
-	context_commentstring = {
-		enable = true,
-		enable_autocmd = false,
-	},
+incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn", -- set to `false` to disable one of the mappings
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
+    },
+  },
 })
