97ba47572a56f3a43fe4459a60d729095baa906c
fvrrrn <boris.chernystrand@gmail.com>
2024-01-17 21:43:56 +0300

    feat(nvim): update editor.lua

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -3,9 +3,19 @@ return {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.5",
     dependencies = { "nvim-lua/plenary.nvim" },
+    -- keys = {
+    --   { "n", "<leader>ff", "<cmd>Telescope find_files<CR>", desc = "Fuzzy find files in cwd" },
+    --   { "n", "<leader>fs", "<cmd>Telescope live_grep<CR>", desc = "Fuzzy find string in cwd" },
+    --   { "n", "<leader>fb", "<cmd>Telescope buffers<CR>", desc = "Find string in cwd" },
+    --   { "n", "<leader>fh", "<cmd>Telescope help_tags<CR>", desc = "Find string under cursor in cwd" },
+    -- },
     config = function()
       local telescope = require("telescope")
-      local builtin = require("telescope.builtin")
+
+      vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Fuzzy find files in cwd" })
+      vim.keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<CR>", { desc = "Fuzzy find string in cwd" })
+      vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<CR>", { desc = "Find string in cwd" })
+      vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", { desc = "Find string under cursor in cwd" })
 
       telescope.setup({
         pickers = {
@@ -14,11 +24,6 @@ return {
           },
         },
       })
-
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Fuzzy find files in cwd" })
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Fuzzy find recent files" })
-      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find string in cwd" })
-      vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Find string under cursor in cwd" })
     end,
   },
   {
@@ -109,6 +114,11 @@ return {
   {
     "smoka7/hop.nvim",
     version = "*",
-    opts = {},
+    -- keys = {
+    --   { "<silent>", "f", "<cmd>HopAnywhere<CR>", desc = "Start search for word start anywhere", silent = true },
+    -- },
+    opts = function()
+      vim.keymap.set("", "f", "<cmd>HopWord<CR>", { desc = "Start search for word start anywhere" })
+    end,
   },
 }
