cf3a880771ccbe1ea45df005fd122c5a75216065
fvrrrn <boris.chernystrand@gmail.com>
2024-01-17 21:43:00 +0300

    feat(nvim): add virtual text to dap

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -289,10 +289,11 @@ return {
     dependencies = {
       "rcarriga/nvim-dap-ui",
       "mxsdev/nvim-dap-vscode-js",
-      -- build debugger from source
+      "theHamsta/nvim-dap-virtual-text",
       {
         "microsoft/vscode-js-debug",
         version = "1.x",
+        -- build debugger from source
         build = "npm i && npm run compile vsDebugServerBundle && mv dist out",
       },
     },
@@ -333,6 +334,30 @@ return {
       require("dap-vscode-js").setup({
         debugger_path = vim.fn.stdpath("data") .. "/lazy/vscode-js-debug",
         adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" },
+      })
+
+      local dap_vt = require("nvim-dap-virtual-text")
+
+      -- ╭──────────────────────────────────────────────────────────╮
+      -- │ DAP Virtual Text Setup                                   │
+      -- ╰──────────────────────────────────────────────────────────╯
+      -- Enable virtual text
+      vim.g.dap_virtual_text = true
+      dap_vt.setup({
+        enabled = true, -- enable this plugin (the default)
+        enabled_commands = true, -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
+        highlight_changed_variables = true, -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
+        highlight_new_as_changed = false, -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
+        show_stop_reason = true, -- show stop reason when stopped for exceptions
+        commented = false, -- prefix virtual text with comment string
+        only_first_definition = true, -- only show virtual text at first definition (if there are multiple)
+        all_references = false, -- show virtual text on all all references of the variable (not only definitions)
+        filter_references_pattern = "<module", -- filter references (not definitions) pattern when all_references is activated (Lua gmatch pattern, default filters out Python modules)
+        -- Experimental Features:
+        virt_text_pos = "eol", -- position of virtual text, see `:h nvim_buf_set_extmark()`
+        all_frames = false, -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
+        virt_lines = false, -- show virtual lines instead of virtual text (will flicker!)
+        virt_text_win_col = nil, -- position the virtual text at a fixed window column (starting from the first text column) ,
       })
 
       for _, language in ipairs({ "typescript", "javascript", "svelte", "typescriptreact", "javascriptreact" }) do
@@ -391,7 +416,28 @@ return {
         }
       end
 
-      require("dapui").setup()
+      require("dapui").setup({
+        controls = {
+          element = "repl",
+          enabled = true,
+          icons = {
+            disconnect = "Disconnect",
+            pause = "Pause",
+            play = "Play",
+            run_last = "run_last",
+            step_back = "step_back",
+            step_into = "step_into",
+            step_out = "step_out",
+            step_over = "step_over",
+            terminate = "terminate",
+          },
+        },
+        icons = {
+          collapsed = "collapsed",
+          current_frame = "current_frame",
+          expanded = "expanded",
+        },
+      })
       local dap, dapui = require("dap"), require("dapui")
       dap.listeners.after.event_initialized["dapui_config"] = function()
         dapui.open({ reset = true })
