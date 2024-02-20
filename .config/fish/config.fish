4d6c5dfa3ec7ed0b20321bf230a1de5da3728bb7
fvrrrn <boris.chernystrand@gmail.com>
2024-02-20 20:57:00 +0300

    feat(fish): set ASCII cursor

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,5 +3,19 @@ set fish_greeting ''
 
 fish_vi_key_bindings
 
-alias git-dtf='/usr/bin/git --git-dir=$HOME/.git-dtf/ --work-tree=$HOME' 
+alias dtf='/usr/bin/git --git-dir=$HOME/.dtf/ --work-tree=$HOME' 
+
+set fish_cursor_default block
+set fish_cursor_insert line
+set fish_cursor_replace_one underscore
+set fish_cursor_visual block
+
+
+set tide_character_icon \x3e
+set tide_character_vi_icon_default \x3e
+set tide_character_vi_icon_replace \x3e
+set tide_character_vi_icon_visual \x3e
+
+
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH $HOME/.ghcup/bin # ghcup-env
 
