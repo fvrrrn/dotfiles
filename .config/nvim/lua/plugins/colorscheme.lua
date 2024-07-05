6e79482006062cc3c41c2b9e8d25659a4b5ee1a8
fvrrrn <boris.chernystrand@gmail.com>
2024-07-05 13:04:48 +0300

    nvim: remove italics from colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -5,6 +5,15 @@ return {
       "rktjmp/lush.nvim",
     },
     config = function()
+-- Apply custom highlights on colorscheme change.
+-- Must be declared before executing ':colorscheme'.
+grpid = vim.api.nvim_create_augroup('custom_highlights', {})
+vim.api.nvim_create_autocmd('ColorScheme', {
+  group = grpid,
+  pattern = '*bones',
+  command = 'hi Comment  gui=NONE |' ..
+            'hi Constant gui=NONE'
+})
       vim.api.nvim_command("colorscheme zenbones")
     end,
   },
